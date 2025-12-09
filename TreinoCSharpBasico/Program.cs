b830ba76b46da914a903ef03e8a2f56d404dd304
Karolaine Pereira <karolainepereira2012@hotmail.com>
2025-12-09 11:34:51 -0300

    Alterações no código

---
+++ TreinoCSharpBasico/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Cadastro simples de produto!");
+
+string nome;
+decimal preco;
+int estoque;
+bool ativo;
+
+string nomeProduto = "Teclado HyperX";
+decimal precoProduto = 380.05m;
+int estoqueProduto = 50;
+bool ativoProduto = true;
+
+System.Console.WriteLine($"Produto: {nomeProduto}");
+System.Console.WriteLine($"Preço: {precoProduto}");
+System.Console.WriteLine($"Estoque: {estoqueProduto}");
+System.Console.WriteLine($"Ativo: {ativoProduto}");
