ec13fc626c768613fef027d00411871cdb234d0a
Karolaine Pereira <karolainepereira2012@hotmail.com>
2026-01-28 10:24:25 -0300

    UpdateDataHora.cs
    Desvendando o datetime

--- TreinoCSharpBasico/Operadores/DataHora.cs
+++ TreinoCSharpBasico/Operadores/DataHora.cs
@@ -12,6 +12,33 @@ class DataHora
         //três dias atrás, horário zerado, desde o início do dia
         //    var threeDaysAgo = today.AddDays(-3);
 
+        var sixMonthsLater = today.AddMonths(6);
+        var twoYearsLater = today.AddYears(2);
+
+
+        var shortDate = now.ToShortDateString();
+        var longTime = now.ToLongTimeString();
+
+        var date = now.Date;
+        var day = now.Day;
+        var month = now.Month;
+        var year = now.Year;
+        var hour = now.Hour;
+        var minute = now.Minute;
+        var second = now.Second;
+
+        var hoje = DateTime.Now.DayOfWeek;
+
+        if (hoje == DayOfWeek.Saturday || hoje == DayOfWeek.Sunday)
+        {
+            System.Console.WriteLine("É fim de semana!");
+        }
+
+        var diaDoAno = DateTime.Now.DayOfYear;
+
+
+
+
 
 
 
