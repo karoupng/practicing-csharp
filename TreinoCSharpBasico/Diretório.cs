66a1888569d5f4d5ff85f0edbb982fbf9a75d578
Karolaine Pereira <karolainepereira2012@hotmail.com>
2026-01-30 12:53:23 -0300

    Create Diretório.cs

---
+++ TreinoCSharpBasico/Diretório.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreinoCSharpBasico
+{
+     class Diretório
+    {
+        public static void Executar()
+        {
+            #region Directory e Directory Info
+
+            //Directory estático para operações rápidas
+            var folderName = "pasta";
+            var subFolderName = "subpasta";
+            var subFolderNameStatic = "subpasta_estatica";
+
+            //Instanciar para reutilizar o objeto, facilitando múltiplas operações.
+
+            var directoryInfoSubFolder = new DirectoryInfo(subFolderName);
+
+            if (!Directory.Exists(folderName) || !directoryInfoSubFolder.Exists)
+            {
+                Directory.CreateDirectory(subFolderNameStatic);
+            }
+
+
+            
+
+
+            #endregion
+
+        }
+    }
+}
